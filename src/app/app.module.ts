17aedb034895ec423e2d5defd97d02354f888558
boaldave <daveb@codeqwik.net>
2021-02-01 13:28:57 -0700

    Defect on new directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,19 @@ import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { FlexContainerComponent } from "./flex-container/flex-container.component";
 import { FlexItemComponent } from "./flex-item/flex-item.component";
-import { FlexContainerFlowDirectionDirective } from './flex-container/flex-container-flow-direction.directive';
-import { FlexContainerJustifyDirective } from './flex-container/flex-container-justify.directive';
-import { FlexContainerVerticalAlignmentDirective } from './flex-container/flex-container-flexContainer-vertical-alignment.directive';
-import { FlexContainerWrapDirective } from './flex-container/flex-container-wrap.directive';
-import { FlexItemOrderDirective } from './flex-item/flex-item-order.directive';
-import { FlexItemRelativeSizeDirective } from './flex-item/flex-item-relative-size.directive';
-import { FlexItemWidthDirective } from './flex-item/flex-item-width-percentage.directive';
+import { FlexContainerFlowDirectionDirective } from "./flex-container/flex-container-flow-direction.directive";
+import { FlexContainerJustifyDirective } from "./flex-container/flex-container-justify.directive";
+import { FlexContainerVerticalAlignmentDirective } from "./flex-container/flex-container-vertical-alignment.directive";
+import { FlexContainerWrapDirective } from "./flex-container/flex-container-wrap.directive";
+import { FlexItemOrderDirective } from "./flex-item/flex-item-order.directive";
+import { FlexItemRelativeSizeDirective } from "./flex-item/flex-item-relative-size.directive";
+import { FlexItemWidthPercentageDirective } from "./flex-item/flex-item-width-percentage.directive";
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
   declarations: [
     AppComponent,
+    HelloComponent,
     FlexContainerComponent,
     FlexContainerFlowDirectionDirective,
     FlexContainerJustifyDirective,
@@ -25,8 +26,8 @@ import { FlexItemWidthDirective } from './flex-item/flex-item-width-percentage.d
     FlexItemComponent,
     FlexItemOrderDirective,
     FlexItemRelativeSizeDirective,
-    FlexItemWidthDirective
+    FlexItemWidthPercentageDirective
   ],
   bootstrap: [AppComponent]
 })
-
+export class AppModule {}
