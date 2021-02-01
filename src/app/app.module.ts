c3bd2862b2f7fef16ed0b4e1c827ba9d232eeb04
boaldave <daveb@codeqwik.net>
2021-02-01 13:23:28 -0700

    Defects on new Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,25 @@ import { HelloComponent } from "./hello.component";
 import { FlexContainerComponent } from "./flex-container/flex-container.component";
 import { FlexItemComponent } from "./flex-item/flex-item.component";
 import { FlexContainerFlowDirectionDirective } from './flex-container/flex-container-flow-direction.directive';
+import { FlexContainerJustifyDirective } from './flex-container/flex-container-justify.directive';
+import { FlexContainerVerticalAlignmentDirective } from './flex-container/flex-container-flexContainer-vertical-alignment.directive';
+import { FlexContainerWrapDirective } from './flex-container/flex-container-wrap.directive';
+import { FlexItemOrderDirective } from './flex-item/flex-item-order.directive';
+import { FlexItemRelativeSizeDirective } from './flex-item/flex-item-relative-size.directive';
+import { FlexItemWidthDirective } from './flex-item/flex-item-width-percentage.directive';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
   declarations: [
     AppComponent,
-    HFlexContainerFlowDirectionDirectiveomponent,
+    FlexContainerComponent,
+    FlexContainerFlowDirectionDirective,
+    FlexContainerJustifyDirective,
+    FlexContainerWrapDirective,
     FlexItemComponent,
-    FlexContainerFlowDirectionDirective
+    FlexItemOrderDirective,
+    FlexItemRelativeSizeDirective,
+    FlexItemWidthDirective
   ],
   bootstrap: [AppComponent]
 })
